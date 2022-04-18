a0c19d0cdc4af1665d8bc9a146369e25094ea471
eycanny <nancy.the.coder@gmail.com>
2022-04-18 12:36:37 -0700

    Create model classes, User and Note

---
+++ model.py
@@ -0,0 +1,55 @@
+"""Models for note-taking app."""
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    #MVP
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    #2.0
+    #username = db.Column(db.String(20), unique=True, nullable=False)
+
+    notes = db.relationship("Note", back_populates="user")
+
+
+class Note(db.Model):
+    """A note."""
+
+    __tablename__ = "notes"
+
+    #MVP
+    note_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    date_created = db.Column(db.DateTime, nullable=False)
+    date_modified = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    user = db.relationship("User", back_populates="notes")
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///notes", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
