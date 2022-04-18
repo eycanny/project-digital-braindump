23fa139846405ace7acf72c1d96bed9aacdc2748
eycanny <nancy.the.coder@gmail.com>
2022-04-18 14:13:45 -0700

    Apply conventions

--- model.py
+++ model.py
@@ -12,17 +12,17 @@ class User(db.Model):
     __tablename__ = "users"
 
     #MVP
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    email = db.Column(db.String, unique = True, nullable = False)
+    password = db.Column(db.String, nullable = False)
 
     #2.0
-    #username = db.Column(db.String(20), unique=True, nullable=False)
+    #username = db.Column(db.String(20), unique = True, nullable = False)
 
-    notes = db.relationship("Note", back_populates="user")
+    notes = db.relationship("Note", back_populates = "user")
 
     def __repr__(self):
-        return f"<User user_id={self.user_id} email={self.email}>"
+        return f"<User user_id = {self.user_id} email = {self.email}>"
 
 
 class Note(db.Model):
@@ -31,20 +31,20 @@ class Note(db.Model):
     __tablename__ = "notes"
 
     #MVP
-    note_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(60))
-    body = db.Column(db.Text)
-    date_created = db.Column(db.DateTime, nullable=False)
-    date_modified = db.Column(db.DateTime)
+    note_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    title = db.Column(db.String(60), nullable = True)
+    body = db.Column(db.Text, nullable = True)
+    date_created = db.Column(db.DateTime, nullable = False)
+    date_modified = db.Column(db.DateTime, nullable = True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
-    user = db.relationship("User", back_populates="notes")
+    user = db.relationship("User", back_populates = "notes")
 
     def __repr__(self):
-        return f"<Note note_id={self.user_id}>"
+        return f"<Note note_id = {self.user_id}>"
 
 
-def connect_to_db(flask_app, db_uri="postgresql:///notes", echo=True):
+def connect_to_db(flask_app, db_uri = "postgresql:///notes", echo = True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
