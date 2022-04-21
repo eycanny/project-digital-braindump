bb8dabf89ab706a0a915326fc9cea99a3d301bbd
eycanny <nancy.the.coder@gmail.com>
2022-04-20 17:13:18 -0700

    Change API source
    - Fixed issue of quote not appearing on page

--- static/js/inspiration.js
+++ static/js/inspiration.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const url = 'https://zenquotes.io/api/quotes';
+// const url = 'https://zenquotes.io/api/quotes';
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(responseData => {
+//         document
+//             .querySelector('#affirmation')
+//             .innerHTML = `<p>"${responseData[0]['h']}"</p>`
+//         });
+
+const url = 'https://quotes.rest/qod?category=inspire&language=en';
 
 fetch(url)
     .then(response => response.json())
     .then(responseData => {
         document
             .querySelector('#affirmation')
-            .innerHTML = `<p>"${responseData[0]['h']}"</p>`
+            .innerHTML = `<p>"${responseData['contents']['quotes'][0]['quote']}"</p>`
         });
