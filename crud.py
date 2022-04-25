44e23af95fec0139e4a89559d36e9e2e9e78afae
eycanny <nancy.the.coder@gmail.com>
2022-04-25 16:48:56 -0700

    Add library and functions, update function
    - Include func, nullslast imports from sqlalchemy library
    - Added functions for sorting notes
    - Removed default values for title and body in create_note() function

--- crud.py
+++ crud.py
@@ -2,6 +2,7 @@
 
 from model import db, User, Note, connect_to_db
 from datetime import datetime
+from sqlalchemy import func, nullslast
 
 
 def create_user(email, password, username):
@@ -30,7 +31,7 @@ def get_user_by_id(user_id):
     return User.query.filter(User.user_id == user_id).first()
 
 
-def create_note(user, title = None, body = None):
+def create_note(user, title, body):
     """Create and return a new note."""
 
     note = Note(
@@ -71,6 +72,42 @@ def get_note_by_keyword(keyword, user_id):
         (Note.title.ilike(f"%{keyword}%")) | (Note.body.ilike(f"%{keyword}%")),
         Note.user_id == user_id
         ).all()
+
+
+def sort_note_by_title_asc(user_id):
+    """Return notes sorted by title in ascending order."""
+
+    return Note.query.filter(Note.user_id == user_id).order_by(nullslast(func.lower(Note.title).asc())).all()
+
+
+def sort_note_by_title_desc(user_id):
+    """Return notes sorted by title in descending order."""
+
+    return Note.query.filter(Note.user_id == user_id).order_by(nullslast(func.lower(Note.title).desc())).all()
+
+
+def sort_note_by_date_created_asc(user_id):
+    """Return notes sorted by date created in ascending order."""
+
+    return Note.query.filter(Note.user_id == user_id).order_by(Note.date_created.asc()).all()
+
+
+def sort_note_by_date_created_desc(user_id):
+    """Return notes sorted by date created in descending order."""
+
+    return Note.query.filter(Note.user_id == user_id).order_by(Note.date_created.desc()).all()
+
+
+def sort_note_by_date_modified_asc(user_id):
+    """Return notes sorted by date created in ascending order."""
+
+    return Note.query.filter(Note.user_id == user_id).order_by(Note.date_modified.asc()).all()
+
+
+def sort_note_by_date_modified_desc(user_id):
+    """Return notes sorted by date created in descending order."""
+
+    return Note.query.filter(Note.user_id == user_id).order_by(Note.date_modified.desc()).all()
 
 
 if __name__ == "__main__":
