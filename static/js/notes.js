3f97f718d71bf3c7a7ca432845d41996b7343ddd
eycanny <nancy.the.coder@gmail.com>
2022-04-25 17:37:14 -0700

    Refactor text in message for readability

--- static/js/notes.js
+++ static/js/notes.js
@@ -5,7 +5,7 @@ const deleteButton = document.querySelector('#delete-button');
 deleteButton.addEventListener('click', (evt) => {
     evt.preventDefault();
 
-    let result = confirm("Are you sure you want to delete this note?"
+    let result = confirm("Are you sure you want to delete this note? "
         + "You won't be able to restore the data once it's been deleted.");
 
     if (result === true) {
