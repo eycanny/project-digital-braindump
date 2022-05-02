7f2226b575984d60a88f0708ca7872165efb2aed
eycanny <nancy.the.coder@gmail.com>
2022-05-02 13:20:29 -0700

    Update event listeners to accept only image types

--- static/js/note_upload.js
+++ static/js/note_upload.js
@@ -6,13 +6,13 @@ const noteBody = document.querySelector("#note-body")
 imageNoteButton.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  noteBody.innerHTML = 'Body<br/>This is for uploading an image as a note. <input type="file" name="note-image">'
-})
+  noteBody.insertAdjacentHTML('beforeend', '<br/>This is for uploading an image as a note. <input type="file" name="note-image" accept="image/*">')
+});
 
 const imageAttachmentButton = document.querySelector("#image-attachment-btn")
 
 imageAttachmentButton.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  noteBody.insertAdjacentHTML('beforeend', '<br/>This is for attaching an image to a note. <input type="file" name="note-attachment">')
-})
+  noteBody.insertAdjacentHTML('beforeend', '<br/>This is for attaching an image to a note. <input type="file" name="note-attachment" accept="image/*">')
+});
