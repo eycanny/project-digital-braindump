8217c908fd1624c97b6cbafa272f17dfa9b9ce49
eycanny <nancy.the.coder@gmail.com>
2022-05-02 18:15:09 -0700

    Fix sematic error for buttons
    - Resolved Issue: If user pressed the button more than one, multiple buttons show up

--- static/js/note_upload.js
+++ static/js/note_upload.js
@@ -1,18 +1,19 @@
 'use strict';
 
-const imageNoteButton = document.querySelector("#image-note-btn")
-const noteBody = document.querySelector("#note-body")
+const imageNoteButton = document.querySelector("#image-note-btn");
+const imageNoteUploader = document.querySelector("#image-note-upload");
 
 imageNoteButton.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  noteBody.insertAdjacentHTML('beforeend', '<br/>This is for uploading an image as a note. <input type="file" name="note-image" accept="image/*">')
+  imageNoteUploader.innerHTML = '<br/>This is for uploading an image as a note. <input type="file" name="note-image" accept="image/*">'
 });
 
-const imageAttachmentButton = document.querySelector("#image-attachment-btn")
+const imageAttachmentButton = document.querySelector("#image-attachment-btn");
+const imageAttachmentUploader = document.querySelector("#image-attachment-upload");
 
 imageAttachmentButton.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  noteBody.insertAdjacentHTML('beforeend', '<br/>This is for attaching an image to a note. <input type="file" name="note-attachment" accept="image/*">')
+  imageAttachmentUploader.innerHTML = '<br/>This is for attaching an image to a note. <input type="file" name="note-attachment" accept="image/*">'
 });
