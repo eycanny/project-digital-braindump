75e4ac9a5df72816b2c454a6b67f11f980add9dd
eycanny <nancy.the.coder@gmail.com>
2022-05-03 21:00:50 -0700

    Initial Commit
    - Separated code from note_upload.js as it caused upload buttons to not work

---
+++ static/js/modal.js
@@ -0,0 +1,16 @@
+const createNoteButton = document.querySelector("#create-note-button");
+const box = document.getElementById("modal-box");
+
+// When user clicks on button, modal pops up
+createNoteButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+
+  box.style.display = "block";
+})
+
+// When user clicks outside of modal, modal closes
+window.addEventListener("click", (evt) => {
+  if (evt.target == box) {
+    box.style.display = "none";
+  }
+})
