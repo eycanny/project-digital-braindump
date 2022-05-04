d05e211e87449736466f8ed0dbf054d58fc657b0
eycanny <nancy.the.coder@gmail.com>
2022-05-03 21:01:41 -0700

    Remove modal-related code
    - Fixed issue where buttons weren't working due to modal-specific code

--- static/js/note_upload.js
+++ static/js/note_upload.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const createNoteButton = document.querySelector("#create-note-button");
-const box = document.getElementById("modal-box");
-
-// When user clicks on button, modal pops up
-createNoteButton.addEventListener("click", (evt) => {
-  evt.preventDefault();
-
-  box.style.display = "block";
-})
-
-// When user clicks outside of modal, modal closes
-window.addEventListener("click", (evt) => {
-  if (evt.target == box) {
-    box.style.display = "none";
-  }
-})
-
 const imageNoteButton = document.querySelector("#image-note-btn");
 const imageNoteUploader = document.querySelector("#image-note-upload");
 
