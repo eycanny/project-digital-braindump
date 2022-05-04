9a95f907281d454095f93e84d56e026e009705eb
eycanny <nancy.the.coder@gmail.com>
2022-05-03 18:35:13 -0700

    Remove unneeded API fetch

--- static/js/inspiration.js
+++ static/js/inspiration.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// const url = 'https://zenquotes.io/api/quotes';
-
-// fetch(url)
-//     .then(response => response.json())
-//     .then(responseData => {
-//         document
-//             .querySelector('#affirmation')
-//             .innerHTML = `<p>"${responseData[0]['h']}"</p>`
-//         });
-
 const url = 'https://quotes.rest/qod?category=inspire&language=en';
 
 fetch(url)
