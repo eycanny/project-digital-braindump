6c42ff17a8ffa3e7ff9a5bd56c502f950ffeec6f
eycanny <nancy.the.coder@gmail.com>
2022-05-04 17:49:01 -0700

    Fix word from 'deletion' to 'delete'

---
+++ static/js/note_editor.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const deleteButton = document.querySelector('#delete-button');
+
+deleteButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    let result = confirm("Are you sure you want to delete this note? "
+        + "You won't be able to restore the data once it's been deleted.");
+
+    if (result === true) {
+        document.querySelector("#delete-confirm")
+                .innerHTML = '<input type="submit" value="Click to Confirm Delete">'
+    }
+
+});
