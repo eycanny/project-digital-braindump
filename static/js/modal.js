b85e9ab4467c1fd0b7268359db697d34752edd61
eycanny <nancy.the.coder@gmail.com>
2022-05-04 19:22:02 -0700

    Add cancel button event listener
    - When user clicks on cancel, modal closes

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,18 +1,27 @@
 'use strict';
 
-const createNoteButton = document.querySelector("#create-note-button");
-const box = document.getElementById("modal-box");
+const createNoteButton = document.querySelector('#create-note-button');
+const box = document.getElementById('modal-box');
 
-// When user clicks on button, modal pops up
-createNoteButton.addEventListener("click", (evt) => {
+// When user clicks on save button, modal pops up
+createNoteButton.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  box.style.display = "block";
+  box.style.display = 'block';
 })
 
 // When user clicks outside of modal, modal closes
-window.addEventListener("click", (evt) => {
+window.addEventListener('click', (evt) => {
   if (evt.target == box) {
-    box.style.display = "none";
+    box.style.display = 'none';
   }
+})
+
+// When user clicks on cancel button, modal closes
+const cancelButton = document.querySelector('#cancel-button');
+
+cancelButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  box.style.display = 'none';
 })
