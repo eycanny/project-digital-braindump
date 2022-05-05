fe88cb56d32621bced48b351377ebe6ae51c95b0
eycanny <nancy.the.coder@gmail.com>
2022-05-04 17:49:54 -0700

    Change file name and add id to elements

--- static/js/note_upload.js
+++ static/js/note_upload.js
@@ -7,7 +7,7 @@ const imageNoteUploader = document.querySelector("#image-note-upload");
 imageNoteButton.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  imageNoteUploader.innerHTML = '<br/>This is for uploading an image as a note and will override anything written in the body textbox. <input type="file" name="note-image" accept="image/*">'
+  imageNoteUploader.innerHTML = '<br/>This is for uploading an image as a note and will override anything written in the body textbox. <input type="file" name="note-image" id="note-image-body" accept="image/*">'
 });
 
 const imageAttachmentButton = document.querySelector("#image-attachment-btn");
@@ -17,5 +17,5 @@ const imageAttachmentUploader = document.querySelector("#image-attachment-upload
 imageAttachmentButton.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  imageAttachmentUploader.innerHTML = '<br/>This is for attaching an image to a note. <input type="file" name="note-attachment" accept="image/*">'
+  imageAttachmentUploader.innerHTML = '<br/>This is for attaching an image to a note. <input type="file" name="note-attachment" id="note-image-attachment" accept="image/*">'
 });
