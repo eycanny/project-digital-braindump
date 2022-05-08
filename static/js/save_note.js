dad7e59ab4814cc87b31a31b15919d4a091bf749
eycanny <nancy.the.coder@gmail.com>
2022-05-08 03:49:37 -0700

    Update attempt to send file data to backend

--- static/js/save_note.js
+++ static/js/save_note.js
@@ -1,59 +1,110 @@
-'use strict';
+// 'use strict';
 
-const saveButton = document.querySelector('#save-button');
+// const saveButton = document.querySelector('#save-button');
 
-// Adds to page without refreshing but unable to pull data in AJAX request
+// // Adds to page without refreshing but unable to pull data in AJAX
 
-// saveButton.addEventListener('click', (evt) => {
-//   evt.preventDefault();
+saveButton.addEventListener('click', (evt) => {
+  // evt.preventDefault();
 
-//   fetch('/create-note', {
-//     method: 'POST',
-//   })
-//     .then((response) => response.json())
-//     .then((responseData) => {
-//         document.querySelector('#note-container').insertAdjacentHTML(
-//             'beforeend',
-//             `<form action="/notes/${responseData.note_id}" class="note">
-//             <button>
-//                 <h2>${responseData.title}</h2>
+  let file = evt.target.parentElement.parentElement.querySelector("#note-image-body").files[0];
+  // let formData = new FormData().append("image", file);
+
+  console.log(file);
+
+  fetch('/create-note', {
+    method: 'POST',
+    mode: "no-cors",
+    body: new FormData().append("file", file),
+    files: file,
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+  })
+    .then((note) => note.json())
+    .then((noteData) => {
+        document.querySelector('#note-container').insertAdjacentHTML(
+            'beforeend',
+            `<form action="/notes/${noteData.note_id}" class="note">
+            <button>
+                <h2>${noteData.title}</h2>
         
-//                 <img src="${responseData.body}"></img>
-//                 <p>${responseData.body}</p>
+                <img src="${noteData.body}"></img>
+                <p>${noteData.body}</p>
 
-//                 <img src="${responseData.image}"></img>
-//             </button>
-//             </form>`
-//             );
-//     });
-// });
+                <img src="${noteData.image}"></img>
+            </button>
+            </form>`
+            );
+    });
+});
 
-// saveButton.addEventListener('click', (evt) => {
-//   evt.preventDefault();
+// // const noteImageUpload = document.getElementById('#note-image');
+// // const noteAttachmentUPload = document.getElementById('#note-attachment');
+// // const formData = new FormData();
 
-//   fetch('/api/image-upload')
-//     .then((images) => images.json())
-//     .then((imageData) => {
-//       const images = imageData;
+// // if (noteImageUpload.files.length = 1) {
+// //   formData.append('note-image', noteImageUpload.files[0]);
+// // }
 
-//       const formInputs = {
-//         title: document.getElementById('#note-title').value,
-//         body: document.getElementById('#note-body').value,
-//         imageBody: images.note_image,
-//         imageAttachment: images.note_attachment,
-//       };
+// // if (noteAttachmentUPload.files.length = 1) {
+// //   formData.append('note-attachment', noteAttachmentUPload.files[0]);
+// // }
 
-//       fetch('/create-note', {
-//         method: 'POST',
-//         body: JSON.stringify(formInputs),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then((responseJson) => {
-//           console.log(responseJson)
-//         });
+// // const upload = (file) => {
+// //   fetch('/create-note',{
+// //     method: 'POST',
+// //     body: noteImageUpload.files[0],
+// //   })
+// // }
 
-//   });
-// });
+// // saveButton.addEventListener('click', (evt) => {
+// //   evt.preventDefault();
+
+// //   const formInputs = {
+// //     title: document.getElementById('#note-title').value,
+// //     body: document.getElementById('#note-body').value,
+// //   };
+
+// //   const fileInputs = {
+// //     imageBody: document.getElementById('#note-image').files,
+// //     imageAttachment: document.getElementById('#note-attachment').files,
+// //   };
+
+// //   Promise.all([
+// //     fetch('/create-note', {
+// //       method: 'POST',
+// //       body: JSON.stringify(formInputs),
+// //       headers: {
+// //       'Content-Type': 'application/json',
+// //       }
+// //     }),
+// //     fetch('/api-image-upload', {
+// //       method: 'POST',
+// //       body: JSON.stringify(fileInputs),
+// //       headers: {
+// //       'Content-Type': 'application/json',
+// //       }
+// //     })
+// //   ])
+// //     .then((responses) => {
+// //       Promise.all(responses.map((response) => {
+// //         response.json();
+// //       }));
+// //     })
+// //     .then((responseData) => {
+// //       document.querySelector('#note-container').insertAdjacentHTML(
+// //         'beforeend',
+// //         `<form action="/notes/${responseData.note_id}" class="note">
+// //         <button>
+// //             <h2>${responseData.title}</h2>
+    
+// //             <img src="${responseData.body}"></img>
+// //             <p>${responseData.body}</p>
+
+// //             <img src="${responseData.image}"></img>
+// //         </button>
+// //         </form>`
+// //         );
+// //     });
+// // });
