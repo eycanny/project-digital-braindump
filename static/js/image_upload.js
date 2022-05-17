2a4a260eba881b322e96f02a6369ed625e850fb1
eycanny <nancy.the.coder@gmail.com>
2022-05-17 15:48:29 -0700

    Decrease size of helper text

--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -7,7 +7,7 @@ const imageNoteUploader = document.querySelector('#image-note-upload');
 imageNoteButton.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  imageNoteUploader.innerHTML = '<p>This is for uploading an image as a note and will override anything written in the body textbox.</p><br/><input type="file" name="note-image" id="note-image-body" accept="image/*">'
+  imageNoteUploader.innerHTML = '<p style="font-size: 12px">This is for uploading an image as a note and will override anything written in the body textbox.</p><br/><input type="file" name="note-image" id="note-image-body" accept="image/*">'
 });
 
 const imageAttachmentButton = document.querySelector('#image-attachment-btn');
@@ -17,5 +17,5 @@ const imageAttachmentUploader = document.querySelector('#image-attachment-upload
 imageAttachmentButton.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  imageAttachmentUploader.innerHTML = '<p>This is for attaching an image to a note.</p><br/><input type="file" name="note-attachment" id="note-image-attachment" accept="image/*">'
+  imageAttachmentUploader.innerHTML = '<p style="font-size: 12px">This is for attaching an image to a note.</p><br/><input type="file" name="note-attachment" id="note-image-attachment" accept="image/*">'
 });
