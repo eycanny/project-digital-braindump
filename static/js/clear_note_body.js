fb41c2656c02dbae1e698c5cd8f0e0b4c76b9549
eycanny <nancy.the.coder@gmail.com>
2022-05-16 17:50:19 -0700

    Update styling of note editor page
    - Fixed headers
    - Fixed width of input fields
    - Reorganized code with rows and columns
    - Updated DOM of uploading buttons
    - Started updating button elements

--- static/js/clear_note_body.js
+++ static/js/clear_note_body.js
@@ -7,5 +7,5 @@ const noteBody = document.querySelector('#note-body');
 clearNoteBodyButton.addEventListener('click', (evt) => {
     evt.preventDefault();
 
-    noteBody.innerHTML = 'Body<br/><textarea type="text" name="body" id="text-body" style="height: 450px; width: 450px"></textarea>'
+    noteBody.innerHTML = '<textarea type="text" name="body" id="text-body" style="height: 450px; width: 100%"></textarea>'
 })
